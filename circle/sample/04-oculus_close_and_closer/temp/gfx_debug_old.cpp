d7fa65a7679d869313b7986d47f7f8d3ac9565f8
bestenfallskongenial <77353739+bestenfallskongenial@users.noreply.github.com>
2026-03-10 21:38:55 +0100

    hmm

--- circle/sample/04-oculus_close_and_closer/temp/gfx_debug_old.cpp
+++ circle/sample/04-oculus_close_and_closer/temp/gfx_debug_old.cpp
@@ -1,18 +1,20 @@
 // ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-// my new gfx log with my own log code
+// my new gfx log with my own logg code
 // ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-void CKernel::shaderLog(char* buffer, u32& index, GLint shader, int shaderIndex)
+void CKernel::shaderLog(GLint shader, int shaderIndex)
 {
     GLint success;
     glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
 
     m_shaderStatusFlags[shaderIndex] = (success == GL_TRUE);
 
-    storeLog(buffer, index, "----------------------------------------------------------------", EMPTYLOG, EMPTYLOG, EMPTYLOG, EMPTYLOG);
-    storeLog(buffer, index, "Program compile status idx/success", (u32)shaderIndex, (u32)success, EMPTYLOG, EMPTYLOG);
+    storeLog("----------------------------------------------------------------");
+    storeLog("Program compile status idx/success",
+             (u32)shaderIndex,
+             (u32)success);
 }
 
-void CKernel::programLog(char* buffer, u32& index, GLint shader, int program_index)
+void CKernel::programLog(GLint shader, int program_index)
 {
     int internal_index = 0;
     if (program_index > 0)
@@ -21,25 +23,29 @@ void CKernel::programLog(char* buffer, u32& index, GLint shader, int program_ind
     GLint success;
     glGetProgramiv(shader, GL_LINK_STATUS, &success);
 
-    storeLog(buffer, index, "----------------------------------------------------------------", EMPTYLOG, EMPTYLOG, EMPTYLOG, EMPTYLOG);
-    storeLog(buffer, index, "Program link status idx/success", (u32)program_index, (u32)success, EMPTYLOG, EMPTYLOG);
+    storeLog("----------------------------------------------------------------");
+    storeLog("Program link status idx/success",
+             (u32)program_index,
+             (u32)success);
 
     char name[27];
     strncpy(name, &m_bufferFsh[program_index][2], 26);
     name[26] = '\0';
 
-    storeMsg(buffer, index, "Program short name", name, 26);
-    storeMsg(buffer, index, "Filename", g_ScnFsh[internal_index], 64);   // same behavior conceptually
-    storeLog(buffer, index, "Program byte size", (u32)g_bytFsh[program_index], EMPTYLOG, EMPTYLOG, EMPTYLOG);
+    storeMsg("Program short name", name, 26);
+    storeMsg("Filename", g_ScnFsh[internal_index], 64);   // same behavior conceptually
+    storeLog("Program byte size",
+             (u32)g_bytFsh[program_index]);
 
     char log[1024];
     glGetProgramInfoLog(shader, sizeof(log), NULL, log);
-    storeMsg(buffer, index, "Program InfoLog", log, sizeof(log));
+    storeMsg("Program InfoLog", log, sizeof(log));
 
     GLint numUniforms;
     glGetProgramiv(shader, GL_ACTIVE_UNIFORMS, &numUniforms);
 
-    storeLog(buffer, index, "Active Uniforms", (u32)numUniforms, EMPTYLOG, EMPTYLOG, EMPTYLOG);
+    storeLog("Active Uniforms",
+             (u32)numUniforms);
 
     for (GLint i = 0; i < numUniforms; ++i)
     {
@@ -51,15 +57,20 @@ void CKernel::programLog(char* buffer, u32& index, GLint shader, int program_ind
         glGetActiveUniform(shader, i, sizeof(uname), &length, &size, &type, uname);
         GLint location = glGetUniformLocation(shader, uname);
 
-        storeLog(buffer, index, "Uniform idx/size/type/loc", (u32)i, (u32)size, (u32)type, (u32)location);
+        storeLog("Uniform idx/size/type/loc",
+                 (u32)i,
+                 (u32)size,
+                 (u32)type,
+                 (u32)location);
 
-        storeMsg(buffer, index, "Uniform name", uname, length);
+        storeMsg("Uniform name", uname, length);
     }
 
     GLint numAttributes;
     glGetProgramiv(shader, GL_ACTIVE_ATTRIBUTES, &numAttributes);
 
-    storeLog(buffer, index, "Active Attributes", (u32)numAttributes, EMPTYLOG, EMPTYLOG, EMPTYLOG);
+    storeLog("Active Attributes",
+             (u32)numAttributes);
 
     for (GLint i = 0; i < numAttributes; ++i)
     {
@@ -71,15 +82,19 @@ void CKernel::programLog(char* buffer, u32& index, GLint shader, int program_ind
         glGetActiveAttrib(shader, i, sizeof(aname), &length, &size, &type, aname);
         GLint location = glGetAttribLocation(shader, aname);
 
-        storeLog(buffer, index, "Attribute idx/size/type/loc", (u32)i, (u32)size, (u32)type, (u32)location);
+        storeLog("Attribute idx/size/type/loc",
+                 (u32)i,
+                 (u32)size,
+                 (u32)type,
+                 (u32)location);
 
-        storeMsg(buffer, index, "Attribute name", aname, length);
+        storeMsg("Attribute name", aname, length);
     }
 
-    storeLog(buffer, index, "----------------------------------------------------------------", EMPTYLOG, EMPTYLOG, EMPTYLOG, EMPTYLOG);
+    storeLog("----------------------------------------------------------------");
 }
 
-void CKernel::gfx_check(char* buffer, u32& index, const char* file, unsigned line)
+void CKernel::gfx_check(const char* file, unsigned line)
 {
     static unsigned error_count = 0;
     static bool summary_written = false;
@@ -92,26 +107,33 @@ void CKernel::gfx_check(char* buffer, u32& index, const char* file, unsigned lin
         CTimer* pTimer = CTimer::Get();
         unsigned ticks = pTimer->GetTicks();
 
-        storeLog(buffer, index, "=== Final System Status ticks/count ===", (u32)ticks, (u32)error_count, EMPTYLOG, EMPTYLOG);
+        storeLog("=== Final System Status ticks/count ===",
+                 (u32)ticks,
+                 (u32)error_count);
 
         GLint value;
 
         glGetIntegerv(GL_CURRENT_PROGRAM, &value);
-        storeLog(buffer, index, "Current Program", (u32)value, EMPTYLOG, EMPTYLOG, EMPTYLOG);
+        storeLog("Current Program", (u32)value);
 
         glGetIntegerv(GL_ACTIVE_TEXTURE, &value);
-        storeLog(buffer, index, "Active Texture Unit", (u32)value, EMPTYLOG, EMPTYLOG, EMPTYLOG);
+        storeLog("Active Texture Unit", (u32)value);
 
         GLint viewport[4];
         glGetIntegerv(GL_VIEWPORT, viewport);
-        storeLog(buffer, index, "Viewport x/y", (u32)viewport[0], (u32)viewport[1], EMPTYLOG, EMPTYLOG);
-        storeLog(buffer, index, "Viewport w/h", (u32)viewport[2], (u32)viewport[3], EMPTYLOG, EMPTYLOG);
+        storeLog("Viewport x/y",
+                 (u32)viewport[0],
+                 (u32)viewport[1]);
+        storeLog("Viewport w/h",
+                 (u32)viewport[2],
+                 (u32)viewport[3]);
 
         GLint fb;
         glGetIntegerv(GL_FRAMEBUFFER_BINDING, &fb);
-        storeLog(buffer, index, "Current Framebuffer", (u32)fb, EMPTYLOG, EMPTYLOG, EMPTYLOG);
+        storeLog("Current Framebuffer",
+                 (u32)fb);
 
-        storeLog(buffer, index, "=== End Status Report ===", EMPTYLOG, EMPTYLOG, EMPTYLOG, EMPTYLOG);
+        storeLog("=== End Status Report ===");
         return;
     }
 
@@ -159,11 +181,14 @@ void CKernel::gfx_check(char* buffer, u32& index, const char* file, unsigned lin
                 break;
         }
 
-        storeLog(buffer, index, "OpenGL Error err/ticks/line", (u32)error, (u32)ticks, (u32)line, EMPTYLOG);
+        storeLog("OpenGL Error err/ticks/line",
+                 (u32)error,
+                 (u32)ticks,
+                 (u32)line);
 
-        storeLog(buffer, index, severity, EMPTYLOG, EMPTYLOG, EMPTYLOG, EMPTYLOG);
-        storeLog(buffer, index, error_str, EMPTYLOG, EMPTYLOG, EMPTYLOG, EMPTYLOG);
-        storeLog(buffer, index, file, EMPTYLOG, EMPTYLOG, EMPTYLOG, EMPTYLOG);
+        storeLog(severity);
+        storeLog(error_str);
+        storeLog(file);
 
         error_count++;
         if (error_count >= ERROR_THRESHOLD)
