cfa813b185f9b2151fdc792fa34aeeaa72afe6c4
bestenfallskongenial <77353739+bestenfallskongenial@users.noreply.github.com>
2026-04-29 12:33:25 +0200

    hmm

--- circle/addon/sensor/buttons.d
+++
@@ -1,9 +0,0 @@
-buttons.o buttons.d: buttons.cpp buttons.h \
- ../../include/circle/gpiomanager.h ../../include/circle/interrupt.h \
- ../../include/circle/bcm2835int.h ../../include/circle/exceptionstub.h \
- ../../include/circle/macros.h ../../include/circle/types.h \
- ../../include/assert.h ../../include/circle/gpiopin.h \
- ../../include/circle/spinlock.h ../../include/circle/sysconfig.h \
- ../../include/circle/memorymap.h ../../include/circle/synchronize.h \
- ../../include/circle/timer.h ../../include/circle/string.h \
- ../../include/circle/stdarg.h ../../include/circle/ptrlist.h
--- circle/addon/sensor/mcp300x.d
+++
@@ -1,6 +0,0 @@
-mcp300x.o mcp300x.d: mcp300x.cpp ../../addon/sensor/mcp300x.h \
- ../../include/circle/spimaster.h ../../include/circle/gpiopin.h \
- ../../include/circle/spinlock.h ../../include/circle/sysconfig.h \
- ../../include/circle/memorymap.h ../../include/circle/synchronize.h \
- ../../include/circle/macros.h ../../include/circle/types.h \
- ../../include/assert.h ../../include/circle/spimasteraux.h
