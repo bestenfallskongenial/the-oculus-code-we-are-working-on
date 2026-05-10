2b0ec09573af59a53c42104352152f633e3fc612
bestenfallskongenial <77353739+bestenfallskongenial@users.noreply.github.com>
2026-05-10 21:31:34 +0200

    ssdf

--- circle/sample/test_01/defs_macro.h
+++ circle/sample/test_01/defs_macro.h
@@ -4,6 +4,17 @@
 #define LOG_BUFFER_SIZE (1024 * 64)
 #define LOGLEVEL        4 //LogDebug
 
+    #define 		FILENAME_KNL			"kernel.img"                         // for logger
+
+    #define         PARTITIONSD	            "emmc1-1"	                         // name partition sd dont confuse with device type    
+    #define         PARTITIONUSB	        "umsd1-1"	
+
+    #define         Filename_SYS_LOG        "LOG-SYS.txt"
+    #define 		FILENAME_GL_LOG			"LOG-GLES.txt"                       // names for my log files 
+    #define 		FILENAME_GLERROR		"LOG-GLER.txt"
+    #define 		FILENAME_VCSM_LOG		"LOG-VCSM.txt"
+    #define 		FILENAME_MMAL_LOG		"LOG-MMAL.txt"
+    
     #define 		LOGLEVEL				4	                                // 0: only panic 
                                                                                 // 1: also errors 
                                                                                 // 2: also warnings 
