121b5fd8811794328df55bd4f55ffcd059ac6c94
bestenfallskongenial <77353739+bestenfallskongenial@users.noreply.github.com>
2026-05-10 22:13:43 +0200

    sdf

--- circle/sample/test_01/defs_member.h
+++ circle/sample/test_01/defs_member.h
@@ -94,6 +94,9 @@ public:
                 uint32_t                m_audio_hold_A;
                 uint32_t                m_audio_hold_B;
 
+                bool                    m_audio_flag_A = false;          
+                bool                    m_audio_flag_B = false;
+
                 uint8_t                 g_modeLengthAdd[MODELEN_FLAG_COUNT] =
                                                                 {
                                                                 MAX_MODES,                  // 5 for now    
