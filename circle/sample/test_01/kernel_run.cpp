e68750bb4c5fad24687e3891aa290dc0054efb37
bestenfallskongenial <77353739+bestenfallskongenial@users.noreply.github.com>
2026-05-11 17:57:05 +0200

    sdfsd

--- circle/sample/test_01/kernel_run.cpp
+++ circle/sample/test_01/kernel_run.cpp
@@ -2,24 +2,23 @@
 
 //  static const char FromKernel[] = "kernel";
 
-TShutdownMode CKernel::Run                              (   void )
+TShutdownMode CKernel::Run(void)
 {
-while (1)
-    {    
-    saveFromBufferM         (   PARTITION_NAME_SD,
-                                make83FileName("TXT"),
-                                m_logBuffer,
-                                m_logBufferIndex );
-    bufferToScreenDrawBuffer(   m_logBuffer,
-                                0,
-                                m_logBufferIndex,
-                                0,
-                                0,
-                                0xFFFFFFFF
-                                );
-    
+                saveFromBufferM         (   PARTITION_NAME_SD,
+                                            make83FileName("TXT"),
+                                            m_logBuffer,
+                                            m_logBufferIndex );
 
-    debug();
-    }
-return ShutdownHalt;
-} 
+                bufferToScreenDrawBuffer(   m_logBuffer,
+                                            0,
+                                            m_logBufferIndex,
+                                            0,
+                                            0,
+                                            0xFFFFFFFF );
+
+                while (1)
+                    {
+                    }
+
+                return ShutdownHalt;
+}
