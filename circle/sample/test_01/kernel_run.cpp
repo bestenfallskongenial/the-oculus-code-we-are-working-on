06ca9b67545e59ce03e447a72f9139134cd45e6c
bestenfallskongenial <77353739+bestenfallskongenial@users.noreply.github.com>
2026-05-11 21:32:18 +0200

    sdf

--- circle/sample/test_01/kernel_run.cpp
+++ circle/sample/test_01/kernel_run.cpp
@@ -9,12 +9,11 @@ TShutdownMode CKernel::Run(void)
                 randomVec8              ( start_time_fps_calculation );
 
                 saveFromBufferM         (   PARTITION_NAME_SD,
-                                            /*make83FileName("TXT"),*/
-                                            "loga.txt",
+                                            make83FileName("TXT"),
                                             m_logBuffer,
                                             m_logBufferIndex );
                 m_Timer.MsDelay(1000);
-
+/*
                 bufferToScreenDrawBuffer(   "save buffer success",
                                             0,
                                             sizeof("save buffer success"),
@@ -31,60 +30,49 @@ TShutdownMode CKernel::Run(void)
                                             0xFFFFFFFF );
 
                 m_Timer.MsDelay(1000); 
-
-m_logBufferIndex = 0;
-m_logBuffer[0] = '\0';
-
-                if (!m_SD_has_load) 
-                    {
-                    wrapper_from_sd();
-                    m_SD_has_load = true;
-                    }
-                if (updateUSB("umsd1") == true && m_USB_has_load == false)
-                    { 
-                    wrapper_load_usb();
-                    m_USB_has_load = true;                            
-                    }
-
-                bufferToScreenDrawBuffer(   "wrapper successful",
-                                            0,
-                                            sizeof("wrapper successful"),
-                                            0,
-                                            0,
-                                            0xFFFFFFFF );
-                m_Timer.MsDelay(1000); 
-
-                            start_time_fps_calculation = m_Timer.GetClockTicks();     
-
-                randomVec8              ( start_time_fps_calculation );            
-
-                saveFromBufferM         (   PARTITION_NAME_SD,
-                                            /*make83FileName("TXT"),*/
-                                            "logb.txt",
-                                            m_logBuffer,
-                                            m_logBufferIndex );
-                m_Timer.MsDelay(1000);    
-
-                bufferToScreenDrawBuffer(   "save buffer success",
-                                            0,
-                                            sizeof("save buffer success"),
-                                            0,
-                                            0,
-                                            0xFFFFFFFF );
-                m_Timer.MsDelay(1000); 
-                
-m_logBufferIndex = 0;
-m_logBuffer[0] = '\0';
-
-                bufferToScreenDrawBuffer(   m_logBuffer,
-                                            0,
-                                            m_logBufferIndex,
-                                            0,
-                                            0,
-                                            0xFFFFFFFF );
-                                         
+*/
                 while (m_resetFlag == false)
                     {
+                    start_time_fps_calculation = m_Timer.GetClockTicks(); 
+
+                    if (!m_SD_has_load) 
+                        {
+                        m_logBufferIndex = 0;
+                        m_logBuffer[0] = '\0';
+
+                        wrapper_from_sd();
+
+                        randomVec8              ( start_time_fps_calculation );            
+
+                        saveFromBufferM         (   PARTITION_NAME_SD,
+                                                    make83FileName("TXT"),
+                                                    m_logBuffer,
+                                                    m_logBufferIndex );
+                        m_Timer.MsDelay(1000);
+
+                        m_SD_has_load = true;
+                        }
+
+                    start_time_fps_calculation = m_Timer.GetClockTicks(); 
+                
+                    if (updateUSB("umsd1") == true && m_USB_has_load == false)
+                        {
+                        m_logBufferIndex = 0;
+                        m_logBuffer[0] = '\0'; 
+
+                        wrapper_load_usb();                    
+
+                        randomVec8              ( start_time_fps_calculation );            
+
+                        saveFromBufferM         (   PARTITION_NAME_SD,
+                                                    make83FileName("TXT"),
+                                                    m_logBuffer,
+                                                    m_logBufferIndex );
+                        m_Timer.MsDelay(1000);    
+
+                        m_USB_has_load = true;                            
+                        }
+
                     }
 
                 return ShutdownHalt;
