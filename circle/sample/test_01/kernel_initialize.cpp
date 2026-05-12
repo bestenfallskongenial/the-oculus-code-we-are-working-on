b51e644eb30358c9b90c27c836b4ead0be690fee
bestenfallskongenial <77353739+bestenfallskongenial@users.noreply.github.com>
2026-05-12 20:14:31 +0200

    asdas

--- circle/sample/test_01/kernel_initialize.cpp
+++ circle/sample/test_01/kernel_initialize.cpp
@@ -86,7 +86,7 @@ boolean			CKernel::Initialize						(	void )
 
                     if (bOK)
                     {
-                            gfx_init_OGL(&m_ogl);
+                            initOGL(&m_ogl);
                     }
                     
                 return bOK;
