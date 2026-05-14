6f6dbf99c660c6762ff06ede049f88490f3424f2
bestenfallskongenial <77353739+bestenfallskongenial@users.noreply.github.com>
2026-05-14 18:30:37 +0200

    dfgf

--- circle/sample/test_01/kernel_run.cpp
+++ circle/sample/test_01/kernel_run.cpp
@@ -83,43 +83,43 @@ TShutdownMode CKernel::Run(void)
                                         "channel 0",g_inOutMatrixInt[0][RAW],
                                         "channel 1",g_inOutMatrixInt[1][RAW],
                                         "channel 2",g_inOutMatrixInt[2][RAW],
-                                        "channel 3",g_inOutMatrixInt[3][RAW])     
+                                        "channel 3",g_inOutMatrixInt[3][RAW]);     
 
                     storeLog(           m_logBuffer, m_logBufferIndex,
                                         "channel 4",g_inOutMatrixInt[4][RAW],
                                         "channel 5",g_inOutMatrixInt[5][RAW],
                                         "channel 6",g_inOutMatrixInt[6][RAW],
-                                        "channel 7",g_inOutMatrixInt[7][RAW])               
+                                        "channel 7",g_inOutMatrixInt[7][RAW]);              
                                         
                     storeLog(           m_logBuffer, m_logBufferIndex,
                                         "channel 0",g_inOutMatrixInt[0][VAL],
                                         "channel 1",g_inOutMatrixInt[1][VAL],
                                         "channel 2",g_inOutMatrixInt[2][VAL],
-                                        "channel 3",g_inOutMatrixInt[3][VAL])     
+                                        "channel 3",g_inOutMatrixInt[3][VAL]);    
 
                     storeLog(           m_logBuffer, m_logBufferIndex,
                                         "channel 4",g_inOutMatrixInt[4][VAL],
                                         "channel 5",g_inOutMatrixInt[5][VAL],
                                         "channel 6",g_inOutMatrixInt[6][VAL],
-                                        "channel 7",g_inOutMatrixInt[7][VAL])                                              
+                                        "channel 7",g_inOutMatrixInt[7][VAL]);                                              
 
                     storeLog(           m_logBuffer, m_logBufferIndex,
                                         "channel 0",g_inOutMatrixFlt[0][VAL],
                                         "channel 1",g_inOutMatrixFlt[1][VAL],
                                         "channel 2",g_inOutMatrixFlt[2][VAL],
-                                        "channel 3",g_inOutMatrixFlt[3][VAL])     
+                                        "channel 3",g_inOutMatrixFlt[3][VAL]);     
 
                     storeLog(           m_logBuffer, m_logBufferIndex,
                                         "channel 4",g_inOutMatrixFlt[4][VAL],
                                         "channel 5",g_inOutMatrixFlt[5][VAL],
                                         "channel 6",g_inOutMatrixFlt[6][VAL],
-                                        "channel 7",g_inOutMatrixFlt[7][VAL])               
+                                        "channel 7",g_inOutMatrixFlt[7][VAL]);               
                                         
                     storeLog(           m_logBuffer, m_logBufferIndex,
                                         "channel 0",g_inOutMatrixInt[0][RND],
                                         "channel 1",g_inOutMatrixInt[1][RND],
                                         "channel 2",g_inOutMatrixInt[2][RND],
-                                        "channel 3",g_inOutMatrixInt[3][RND])     
+                                        "channel 3",g_inOutMatrixInt[3][RND]);     
 
                     storeLog(           m_logBuffer, m_logBufferIndex,
                                         "channel 4",g_inOutMatrixInt[4][RND],
@@ -131,13 +131,13 @@ TShutdownMode CKernel::Run(void)
                                         "channel 0",g_inOutMatrixFlt[0][RND],
                                         "channel 1",g_inOutMatrixFlt[1][RND],
                                         "channel 2",g_inOutMatrixFlt[2][RND],
-                                        "channel 3",g_inOutMatrixFlt[3][RND])     
+                                        "channel 3",g_inOutMatrixFlt[3][RND]);     
 
                     storeLog(           m_logBuffer, m_logBufferIndex,
                                         "channel 4",g_inOutMatrixFlt[4][RND],
                                         "channel 5",g_inOutMatrixFlt[5][RND],
                                         "channel 6",g_inOutMatrixFlt[6][RND],
-                                        "channel 7",g_inOutMatrixFlt[7][RND])                                                 
+                                        "channel 7",g_inOutMatrixFlt[7][RND]);                                                
 
                     bufferScreenDraw(   m_logBuffer,
                                         0,
