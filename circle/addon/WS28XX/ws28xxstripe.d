c29ba428113241d223ccf60e4391c4ea5767bae9
bestenfallskongenial <77353739+bestenfallskongenial@users.noreply.github.com>
2026-05-15 14:54:32 +0200

    dasda

---
+++ circle/addon/WS28XX/ws28xxstripe.d
@@ -0,0 +1,6 @@
+ws28xxstripe.o ws28xxstripe.d: ws28xxstripe.cpp ws28xxstripe.h \
+ ../../include/circle/spimaster.h ../../include/circle/gpiopin.h \
+ ../../include/circle/spinlock.h ../../include/circle/sysconfig.h \
+ ../../include/circle/memorymap.h ../../include/circle/synchronize.h \
+ ../../include/circle/macros.h ../../include/circle/types.h \
+ ../../include/assert.h ../../include/circle/util.h
