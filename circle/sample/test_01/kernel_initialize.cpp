e6b6d62f3b4659bbc5efb23682b1b3f800a3a523
bestenfallskongenial <77353739+bestenfallskongenial@users.noreply.github.com>
2026-05-15 07:27:14 +0200

    svd

--- circle/sample/test_01/kernel_initialize.cpp
+++ circle/sample/test_01/kernel_initialize.cpp
@@ -7,12 +7,12 @@ boolean			CKernel::Initialize						(	void )
                 if (bOK)
                     {
                     bOK = m_Interrupt.Initialize();
-                    storeLog( m_logBuffer, m_logBufferIndex, "step",0)
+                    storeLog( m_logBuffer, m_logBufferIndex, "step",0);
                     }       
                 if (bOK)
                     {
                     bOK = m_Timer.Initialize();
-                    storeLog( m_logBuffer, m_logBufferIndex, "step",1)
+                    storeLog( m_logBuffer, m_logBufferIndex, "step",1);
                     }
 
                 if (bOK)
@@ -20,88 +20,88 @@ boolean			CKernel::Initialize						(	void )
                     CLogger::SetRawSink(CKernel::LoggerSink, this);
 
                     bOK = m_Logger.Initialize(0);
-                    storeLog( m_logBuffer, m_logBufferIndex, "step",2)
+                    storeLog( m_logBuffer, m_logBufferIndex, "step",2);
                     }
                 if (bOK)
                     {
                     bOK = startupScreen();
-                    storeLog( m_logBuffer, m_logBufferIndex, "step",3)
+                    storeLog( m_logBuffer, m_logBufferIndex, "step",3);
                 //  msDelay(1000);
                     }
 
                 if (bOK)
                     {
                     bOK = frameBufferInit();
-                    storeLog( m_logBuffer, m_logBufferIndex, "step",4)
+                    storeLog( m_logBuffer, m_logBufferIndex, "step",4);
                     }
 
                 if (bOK)
                     {
                     bOK = m_EMMC.Initialize();
-                    storeLog( m_logBuffer, m_logBufferIndex, "step",5)
+                    storeLog( m_logBuffer, m_logBufferIndex, "step",5);
                 //  msDelay(1000); 
                     }
 
                 if (bOK)
                     {
                     bOK = m_USBHCI.Initialize();
-                    storeLog( m_logBuffer, m_logBufferIndex, "step",6)
+                    storeLog( m_logBuffer, m_logBufferIndex, "step",6);
                 //  msDelay(1000); 
                     }
                     
                 if (bOK)
                     {
                     m_USBHCI.UpdatePlugAndPlay(); 
-                    storeLog( m_logBuffer, m_logBufferIndex, "step",7)
+                    storeLog( m_logBuffer, m_logBufferIndex, "step",7);
                 //  msDelay(1000);
                     }
 
                 if (bOK)
                     {
                     bOK =   wrapperInitDMA();
-                    storeLog( m_logBuffer, m_logBufferIndex, "step",8)
+                    storeLog( m_logBuffer, m_logBufferIndex, "step",8);
                     }
 
                 if (bOK)
                     {
                     bOK =   wrapperInitMEM();
-                    storeLog( m_logBuffer, m_logBufferIndex, "step",9)
+                    storeLog( m_logBuffer, m_logBufferIndex, "step",9);
                     }
 
                 if (bOK)
                     {
                     bOK =   m_VCHIQ.Initialize();
-                    storeLog( m_logBuffer, m_logBufferIndex, "step",10)
+                    storeLog( m_logBuffer, m_logBufferIndex, "step",10);
                     }
 
                     if (bOK)
                     {
                             bcm_host_init();
-                    storeLog( m_logBuffer, m_logBufferIndex, "step",11)
+                    storeLog( m_logBuffer, m_logBufferIndex, "step",11);
                     }    
 
                     if (bOK)
                     {
                             initOGL(&m_ogl);
-                    storeLog( m_logBuffer, m_logBufferIndex, "step",12)
+                    storeLog( m_logBuffer, m_logBufferIndex, "step",12);
                     }
 
                 if (bOK)
                     {
                     bOK =   SPI_init();
-                    storeLog( m_logBuffer, m_logBufferIndex, "step",13)
+                    storeLog( m_logBuffer, m_logBufferIndex, "step",13);
                     }
 
                 if (bOK)
                     {
                     bOK =   SMI_Init(LED_PIN);        // example GPIO18, choose your actual WS2812 SMI GPIO
-                    storeLog( m_logBuffer, m_logBufferIndex, "step",14)
+                    storeLog( m_logBuffer, m_logBufferIndex, "step",14);
                     }
 
                 if (bOK)
                     {
                     bOK =   WS2812_Init(LED_COUNT);      // example LED count, choose your actual count
-                    storeLog( m_logBuffer, m_logBufferIndex, "step",15)
+                    storeLog( m_logBuffer, m_logBufferIndex, "step",15);
                     }
 
                 return bOK;
