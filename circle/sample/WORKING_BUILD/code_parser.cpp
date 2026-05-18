992f6ee66c54976a1a10ad34cb99ecc5e696f3c8
bestenfallskongenial <77353739+bestenfallskongenial@users.noreply.github.com>
2026-05-18 17:45:32 +0200

    sdf

--- circle/sample/WORKING_BUILD/code_parser.cpp
+++ circle/sample/WORKING_BUILD/code_parser.cpp
@@ -1,7 +1,7 @@
 #include "kernel.h"
 
-#define MY_BUFFER   m_logBuffer
-#define MY_INDEX    m_logBufferIndex
+#define MY_BUFFER   m_logParseBuffer
+#define MY_INDEX    m_logParseBufferIndex
 
 bool            CKernel::BMPparser                  (   tex_state*  t,
                                                         char*       p_buffer_array[],
