02bace22bb5d03800ed1c37d0bd9a1af3783ccde
bestenfallskongenial <77353739+bestenfallskongenial@users.noreply.github.com>
2026-05-18 22:54:15 +0200

    sdfs

--- circle/sample/WORKING_BUILD/code_wrappers.cpp
+++ circle/sample/WORKING_BUILD/code_wrappers.cpp
@@ -284,6 +284,26 @@ void            CKernel::wrapper_init_gl_sd         (   )
                                     filecounter[FT_OMT][FLD_PREV],                  // for the continuous loading between devices - lower bound
                                     filecounter[FT_OMT][FLD_LOADED]);               // for the continuous loading between devices - upper bound
 
+                BMPparser       (   &m_tex,
+                                    m_bufferTex,
+                                    g_ScnTex,
+                                    g_bytTex,
+                                    filecounter[FT_TEX][FLD_SIZE],
+                                    filecounter[FT_TEX][FLD_PREV],
+                                    filecounter[FT_TEX][FLD_LOADED]);      
+
+                parse264        (   &m_vid,
+                                    m_videoBlockBase,
+                                    m_bufferVid,
+                                    g_ScnVid,
+                                    g_bytVid,
+                                    filecounter[FT_VID][FLD_PREV],
+                                    filecounter[FT_VID][FLD_LOADED],
+                                    MAX_VIDEO_WIDTH,
+                                    MAX_VIDEO_HEIGHT,
+                                    MAX_VIDEO_PROFILE,
+                                    MAX_VIDEO_LEVEL);                                    
+
                 initVbuffer     (   &m_ogl,
                                     &m_vtx );
 
@@ -362,6 +382,14 @@ void            CKernel::wrapper_init_gl_sd         (   )
 
 void            CKernel::wrapper_init_gl_usb        (   )
 {
+                BMPparser       (   &m_omt,                                         // the dedicated struct for the overlay texture
+                                    m_bufferOmt,                                    // the actual mem-buffer where i have stored it
+                                    g_ScnOmt,
+                                    g_bytOmt,                                       // the array where i stored the loaded bytes
+                                    filecounter[FT_OMT][FLD_SIZE],                  // upper bound for the size
+                                    filecounter[FT_OMT][FLD_PREV],                  // for the continuous loading between devices - lower bound
+                                    filecounter[FT_OMT][FLD_LOADED]);               // for the continuous loading between devices - upper bound
+    
                 BMPparser       (   &m_tex,
                                     m_bufferTex,
                                     g_ScnTex,
