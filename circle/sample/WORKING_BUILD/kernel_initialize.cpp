1865835597ee3aaef5be80ce5afda128082ddd6e
bestenfallskongenial <77353739+bestenfallskongenial@users.noreply.github.com>
2026-05-19 10:14:16 +0200

    sdfsd

--- circle/sample/WORKING_BUILD/kernel_initialize.cpp
+++ circle/sample/WORKING_BUILD/kernel_initialize.cpp
@@ -3,8 +3,8 @@
 //  #undef  __DEBUG_LOG__
     #define __DEBUG_LOG__
 
-#define MY_BUFFER   m_logBuffer
-#define MY_INDEX    m_logBufferIndex    
+// #define MY_BUFFER   m_logBuffer
+// #define MY_INDEX    m_logBufferIndex    
 
 boolean			CKernel::Initialize						(	void )
 {
