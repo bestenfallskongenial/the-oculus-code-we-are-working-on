16ead59e66fc751550694b76f0d41c05cbd07896
bestenfallskongenial <77353739+bestenfallskongenial@users.noreply.github.com>
2026-05-19 10:28:18 +0200

    kam

--- circle/sample/WORKING_BUILD/o_c_setup.h
+++ circle/sample/WORKING_BUILD/o_c_setup.h
@@ -11,7 +11,7 @@
     #define         OMF_SD             		    1	// max number of fragment shader on sd
     #define         FSH_SD             		    1	// max number of fragment shader on sd
     #define         OMT_SD             		    1	// max number of fragment shader on sd
-    #define         TEX_SD             		    0	// max number of textures on sd
+    #define         TEX_SD             		    2	// max number of textures on sd
     #define         VID_SD             		    0	// max number of videos on sd
     #define         KLN_SD                      1
 
@@ -65,7 +65,7 @@
 
     #define         FRM_SIZ                     (1024*1024)         // 1mb
 
-    #define         LOG_SIZ                     (1024*256)          // 256kb
+    #define         LOG_SIZ                     (1024*32)          // 256kb
 
     #define         ADC_SELECT_PRG              7   // we will map this modes to input channels later
     #define         ADC_SELECT_TEX              6   // this are placeholder mactos
