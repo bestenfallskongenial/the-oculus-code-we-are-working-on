f105ff17b182c05205f0d2b99e9d545f47c39f0c
bestenfallskongenial <77353739+bestenfallskongenial@users.noreply.github.com>
2026-05-24 11:33:15 +0200

    asdsa

--- circle/sample/WORKING_BUILD/kernel.h
+++ circle/sample/WORKING_BUILD/kernel.h
@@ -492,7 +492,7 @@ public:
                                                                     MMAL_Port_Parameter_Set_Msg&    tx, 
                                                                     MMAL_Port_Parameter_Set_Reply&  rx);
 
-                bool        setZeroCopyModeMMALOK       (           u32                             port_handle);                                                                    
+                bool        setZeroCopyModeMMALOK       (           u32  port_handle);                                                                    
                                                                     
                 bool        enablePortMMAL              (   const   MMAL_Port_Info_Get_Reply&       src, 
                                                                     MMAL_Port_Action_Msg&           tx, 
