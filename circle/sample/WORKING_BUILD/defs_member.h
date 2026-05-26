8877089029708ab892ac0477893a613a6e1b3814
bestenfallskongenial <77353739+bestenfallskongenial@users.noreply.github.com>
2026-05-26 19:40:51 +0200

    asdsa

--- circle/sample/WORKING_BUILD/defs_member.h
+++ circle/sample/WORKING_BUILD/defs_member.h
@@ -332,10 +332,10 @@ const char* m_debug_table[16]   =
 /*
 u32             m_logBufferIndexLast = 0;
 u32             m_bufferLogIndexLast[LOG_SD+LOG_USB] = { 0 };
-
+*/
 char            m_screenLogBuffer[SCREEN_LOG_BUFFER_SIZE] = { 0 };
 u32             m_screenLogBufferIndex = 0;
-*/
+
 u32             m_logScreenStartIndex = 0;
 
 // VCSM predefined messages as public member
