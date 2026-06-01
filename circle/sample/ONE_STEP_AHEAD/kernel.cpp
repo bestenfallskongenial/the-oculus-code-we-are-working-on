8f5a7dba69e4ba935e77499e9015c1d77b89df38
bestenfallskongenial <77353739+bestenfallskongenial@users.noreply.github.com>
2026-06-01 17:57:36 +0300

    yxcyx

--- circle/sample/ONE_STEP_AHEAD/kernel.cpp
+++ circle/sample/ONE_STEP_AHEAD/kernel.cpp
@@ -1,5 +1,5 @@
 	#include "kernel.h"
-
+/*
 				CKernel::CKernel						(	void )
 				:	m_Interrupt         (),
                     m_Timer             (   &m_Interrupt ),
@@ -20,6 +20,29 @@
                     m_resetFlag         (   false )
 {                    
                 m_ActLED.Blink(1);
+}
+*/
+CKernel::CKernel(void)
+:   m_Interrupt         (),
+    m_Timer             (&m_Interrupt),
+    m_Logger            (LOGLEVEL, &m_Timer),
+    gE_FrameBuffer      (m_Options.GetWidth(),
+                         m_Options.GetHeight(),
+                         32,
+                         0,
+                         TRUE),
+
+    m_EMMC              (&m_Interrupt, &m_Timer, &m_ActLED),
+    m_USBHCI            (&m_Interrupt, &m_Timer, TRUE),
+    m_VCHIQ             (CMemorySystem::Get(), &m_Interrupt),
+
+    m_SMITxDMA          (DMA_CHANNEL_LITE),
+
+    m_pFileSystem       (0),
+    m_bStorageAttached  (FALSE),
+    m_resetFlag         (false)
+{
+    m_ActLED.Blink(1);
 }
 
 				CKernel::~CKernel						(	void )
