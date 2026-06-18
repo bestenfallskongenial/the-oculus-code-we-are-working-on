ada822dbfafdf956bd302a74540f3e36b85dbe2d
bestenfallskongenial <77353739+bestenfallskongenial@users.noreply.github.com>
2026-06-19 02:08:31 +0300

    asdas

--- circle/sample/ONE_STEP_AHEAD/kernel.h
+++ circle/sample/ONE_STEP_AHEAD/kernel.h
@@ -322,6 +322,8 @@ public:
                 void        randomVec8                  (               uint32_t                        p_seed);            // +++
 
                 void        calculate1BPM               (               int                             p_source, 
+                                                                        int                             p_timeBuffer,
+                                                                        int                             p_deltaBuffer,
                                                                         unsigned long                   p_triggerTimeClock);
                 void        predict1Beat                (               int                             p_source, 
                                                                         int                             p_lfoMult);
