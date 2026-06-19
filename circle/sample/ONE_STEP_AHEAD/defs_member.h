70d17b94a58fb868334428fc5af553c1d27840bd
bestenfallskongenial <77353739+bestenfallskongenial@users.noreply.github.com>
2026-06-19 17:50:26 +0300

    asda

--- circle/sample/ONE_STEP_AHEAD/defs_member.h
+++ circle/sample/ONE_STEP_AHEAD/defs_member.h
@@ -101,7 +101,7 @@ public:         // Logging
                 unsigned                        g_currentTime;
 
                 int                             attenuation = 0;
-                bool                            m_audio_mode_activated                          = false;
+                bool                            m_audio_mode_activated                          = true;
 
                 bool                            is_audio[2];
 
