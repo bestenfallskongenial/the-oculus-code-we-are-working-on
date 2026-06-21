56add94fe41f66114fa4296607d96bd35d04efb2
bestenfallskongenial <77353739+bestenfallskongenial@users.noreply.github.com>
2026-06-21 11:57:10 +0300

    asdas

--- circle/sample/ONE_STEP_AHEAD/code_debug.cpp
+++ circle/sample/ONE_STEP_AHEAD/code_debug.cpp
@@ -208,7 +208,7 @@ void CKernel::logButtonStatesRuntime(void)
 
     storeLog(   f_logBuffer, f_logIndex,
                 "mapping",
-                ((g_inOutMatrixInt[ADC_SELECT_PRG][OUT] * (filecounter[FT_FSH][FLD_VALID] -1 ) ) >> 10)
+                ((g_inOutMatrixInt[ADC_SELECT_PRG][RAW] * (filecounter[FT_FSH][FLD_VALID] -1 ) ) >> 10)
                 );
 
     storeLog(   f_logBuffer, f_logIndex,
