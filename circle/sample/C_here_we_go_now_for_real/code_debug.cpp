0e738aa2566a4f304c1d338f738dca49884bc2e9
bestenfallskongenial <77353739+bestenfallskongenial@users.noreply.github.com>
2026-08-04 21:47:06 +0300

    asdasd

--- circle/sample/C_here_we_go_now_for_real/code_debug.cpp
+++ circle/sample/C_here_we_go_now_for_real/code_debug.cpp
@@ -121,25 +121,27 @@ bool            CKernel::startupScreen( void )
                 unsigned fbHeight       =  gE_FrameBuffer.GetHeight();
 
                 storeLogU32(    MY_BFR, MY_IDX, "--------------------------------------------------", X_VAL);
+                storeLogU32(    MY_BFR, MY_IDX, "OCULUS MENDAX Firmware", OS_VERSION, ".", OS_REVISION ); 
+                storeLogU32(    MY_BFR, MY_IDX, "--------------------------------------------------", X_VAL);
                 storeLogU32(    MY_BFR, MY_IDX, machineName, X_VAL, socName, X_VAL, "V", modelMajor, ".", modelRevision);
                 nextline(       MY_BFR, MY_IDX);
-                storeLogU32(    MY_BFR, MY_IDX, "RAM Size", ramSize, "MB" ); 
+                storeLogU32(    MY_BFR, MY_IDX, "RAM Size       ", ramSize, "MB" ); 
             //  storeLogU32(    MY_BFR, MY_IDX, "CPU Speed Mode", cpuSpeedMode );
-                storeLogU32(    MY_BFR, MY_IDX, "CPU Speed Mode ", X_VAL, cpuSpeedMode ? "FAST" : "NORMAL" );
-                storeLogU32(    MY_BFR, MY_IDX, "SoC Max Temp", socMaxTemp, "Celsius" );
+                storeLogU32(    MY_BFR, MY_IDX, "CPU Speed Mode ", X_VAL, cpuSpeedMode ? "Fast" : "Normal" );
+                storeLogU32(    MY_BFR, MY_IDX, "SoC Max Temp   ", socMaxTemp, "Celsius" );
                 nextline(       MY_BFR, MY_IDX);
                 storeLogU32(    MY_BFR, MY_IDX, "Clock CORE", coreClock, "MHz /ARM", armClock, "MHz" );
                 storeLogU32(    MY_BFR, MY_IDX, "EMMC", emmcClock, "MHz /EMMC2", emmc2Clock, "Mhz" );
                 nextline(       MY_BFR, MY_IDX);
-                storeLogU32(    MY_BFR, MY_IDX, "Clock UART MHz", uartClock );
-                storeLogU32(    MY_BFR, MY_IDX, "DMA Channels", dmaChannel );
-                storeLogU32(    MY_BFR, MY_IDX, "USB Delay     ", usbDelay, "ms");
-                storeLogU32(    MY_BFR, MY_IDX, "USB Speed", usbSpeed);
-                storeLogU32(    MY_BFR, MY_IDX, "USB Speed ", X_VAL, usbSpeed ? "FULL 12 Mbps" : "HIGH 480 Mbps" );
+                storeLogU32(    MY_BFR, MY_IDX, "Clock UART     ", uartClock, "MHz" );
+                storeLogU32(    MY_BFR, MY_IDX, "DMA Channels   ", dmaChannel );
+                storeLogU32(    MY_BFR, MY_IDX, " USB Delay     ", usbDelay, "ms");
+            //  storeLogU32(    MY_BFR, MY_IDX, "USB Speed", usbSpeed);
+                storeLogU32(    MY_BFR, MY_IDX, "USB Speed     ", X_VAL, usbSpeed ? " 12 Mbps" : "480 Mbps" );
                 nextline(       MY_BFR, MY_IDX);
-                storeLogU32(    MY_BFR, MY_IDX, "FB Screen", fbWidth, "to", fbHeight, " X/Y" );  
+                storeLogU32(    MY_BFR, MY_IDX, "FB Screen", fbWidth, "/", fbHeight, "Pixel" );  
                 nextline(       MY_BFR, MY_IDX);
-                storeLogU32(    MY_BFR, MY_IDX, "OCULUS MENDAX Firmware", OS_VERSION, ".", OS_REVISION ); 
+
                 storeLogU32(    MY_BFR, MY_IDX, "--------------------------------------------------", X_VAL);
                 nextline(       MY_BFR, MY_IDX);                            
 
